38a1c9cbcd3a24c9fa1d0c51e9f75d74382358e2
Payton Jachec <harrisonjachec@usf.edu>
2026-08-26 10:46:15 -0400

    Add main.py via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+##########################################
+# Payton Jachec # harrisonjachec@usf.edu #
+#----------------------------------------#
+# main.py: prints "Hello, World!" and    #
+# examines behavior of data types.       #
+##########################################
+
+""" we want this script to PRINT the string Hello, World!
+ """
+print("Hello, World!")
+
+a = "hello world!"
+print(a)
+
